4efaca4a4ebb3be3c90d81079df29694240f6abc
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2019-11-22 15:20:13 -0500

    Added make maze button.
    A maze will randomly generate everytime the button is pressed.

--- maze_generation.js
+++ maze_generation.js
@@ -1,28 +1,31 @@
 var stack = [];
 
 function depthFirstRecursiveBacktracking(){
-    current.isWall = false;
-    // Step 1
-    var next = current.checkNeightbors();
-    if(next){
-        next.isWall = false;
-        // Step 2
-        stack.push(current);
-        // Step 3
-        removeWalls(current, next);
-        // Step 4 
-        current = next;
-    }else if(stack.length > 0) {
-        current = stack.pop();
+    wallGrid();
+    current = grid[Math.floor(Math.random()*grid.length)];
+    stack.push(current);
+    while(stack.length != 0){
+        current.isWall = false;
+        // Step 1
+        var next = current.checkNeightbors();
+        if(next){
+            next.isWall = false;
+            // Step 2
+            stack.push(current);
+            // Step 3
+            removeWalls(current, next);
+            // Step 4 
+            current = next;
+        }else if(stack.length > 0) {
+            current = stack.pop();
+        }
     }
+
 }
 
 function removeWalls(a, b){
     var x = a.i - b.i;
     var y = a.j - b.j;
-    console.log(a);
-    console.log(b);
-    console.log(x + ", " + y);
     if(x === 0 && y === 2){
         grid[index(a.i, a.j-1)].isWall = false;
     }else if (y === -2){
@@ -34,6 +37,10 @@ function removeWalls(a, b){
         grid[index(a.i-1, a.j)].isWall = false;
 
     }
+}
 
-
+function wallGrid(){
+    for(var i = 0; i < grid.length; i++){
+        grid[i].isWall = true;
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var cols, rows;
-var w = 10;
+var w = 20;
 var grid = [];
 
 function setup(){
@@ -10,21 +10,18 @@ function setup(){
     for(var j = 0; j < rows; j++){
         for(var i = 0; i < cols; i++){
             var cell = new Cell(i, j);
-            // if(i == 0 || j == cols-1 || i == cols-1 || j == 0){
-            //     cell.isWall=true;
-            // }
             grid.push(cell);
         }
     }
-    current = grid[0];
+    makeMazeButton = createButton("Make Maze");
+    makeMazeButton.mousePressed(depthFirstRecursiveBacktracking);
 }
 
 function draw(){
-    background(51);
     for(var i = 0; i < grid.length; i++){
         grid[i].show();
     }
-    depthFirstRecursiveBacktracking();
+
 }
 
 function mouseClicked(){
