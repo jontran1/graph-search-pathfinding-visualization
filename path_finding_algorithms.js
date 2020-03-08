88c8612b1ff5dd28cdbd4cb0e64f8d4c20c8ff70
jontran1 <jtran001@citymail.cuny.edu>
2020-03-08 15:57:09 -0400

    A* pathing finding algo is implemented. Shortest path is can be found

--- path_finding_algorithms.js
+++ path_finding_algorithms.js
@@ -33,7 +33,7 @@ function dijkstra_path_finding(){
     console.log(Q.size);
     if(Q.size > 0){
         // Get the index cell with the min distance.
-        origin = getCellWithMinDistance();
+        origin = getCellWithMinDistance(distance);
 
         /**
          * If origin is undefined. The remaining cells in Q set is inaccessible
@@ -43,7 +43,8 @@ function dijkstra_path_finding(){
          */
         if(!origin){
             Q.clear(); return;
-        }
+        }else origin.highlightCell();
+
         // Remove origin from set Q.
         Q.forEach(function(cell){
             if(origin.equals(cell)){
@@ -58,7 +59,7 @@ function dijkstra_path_finding(){
             for(i = 0; i < adjacentCells.length; i++){
                 adjacentCell = adjacentCells[i];
                 distanceToAdjacent = distance.get(origin) + getEuclideanDistance(origin, adjacentCell);
-                adjacentCell.highlightCell();
+
                 if(distanceToAdjacent < distance.get(adjacentCell)){
                     distance.set(adjacentCell, distanceToAdjacent);
                     prev.set(adjacentCell, origin);
@@ -90,14 +91,90 @@ function getEuclideanDistance(u, v){
  * Uses a linear search to find the min dsitance. 
  * Should consider getting better performance with a min heap data structure. 
  */
-function getCellWithMinDistance(){
+function getCellWithMinDistance(map){
     tempMin = Number.MAX_VALUE;
     minCell = undefined;
-    for(let[cell, dist] of distance){
+    for(let[cell, dist] of map){
         if(Q.has(cell) && dist < tempMin){
             tempMin = dist;
             minCell = cell;
         }
     }
     return minCell;
+}
+
+function heuristic(cell){
+    if(targetCell && cell) return getEuclideanDistance(cell, targetCell);
+    return undefined;
+}
+
+var gScore;
+var fScore;
+
+function setupA_Star(){
+    Q = new Set();
+    prev = new Map();
+    gScore = new Map();
+    fScore = new Map();
+    playA_StarAnimation = true;
+
+    for(i = 0; i < grid.length; i++){
+        cell = grid[i];
+        if(!cell.isWall){
+            // Set all distances to the largest possible value.
+            gScore.set(cell, Infinity);
+            fScore.set(cell, Infinity);
+            // Set all previous to null.
+            prev.set(cell, undefined);
+            // Add cell to set.
+            Q.add(cell);
+        }
+    }
+
+    gScore.set(startCell, 0);
+    fScore.set(startCell, heuristic(startCell));
+
+}
+
+function aStar(){
+    if(Q.size > 0){
+        // Get the node with the lowest fScore value.
+        current = getCellWithMinDistance(fScore);
+        if(current === targetCell){
+            console.log("a star found target cell");
+            temp = targetCell;
+            while(temp !== startCell){
+                temp = prev.get(temp);
+                if(!temp) return;
+                if(temp !== startCell) temp.highlightCell();
+            }
+            return;
+        }
+
+        // Remove origin from set Q.
+        Q.forEach(function(cell){
+            if(current.equals(cell)){
+                Q.delete(cell);
+            }
+        })
+        current.highlightCell();
+        adjacentCells = current.adjacentCells();
+        
+        if(adjacentCells){
+            for(i = 0; i < adjacentCells.length; i++){
+                adjacentCell = adjacentCells[i];
+                tentative_gScore = gScore.get(current) + getEuclideanDistance(current, adjacentCell);
+
+                if(tentative_gScore < gScore.get(adjacentCell)){
+                    prev.set(adjacentCell, current);
+                    gScore.set(adjacentCell, tentative_gScore);
+                    fScore.set(adjacentCell, gScore.get(adjacentCell) + heuristic(adjacentCell));
+                    if(!Q.has(adjacentCell)){
+                        Q.add(adjacentCell);
+                    }
+                }
+            }
+        }
+
+    }
 }
