2c30ff81e2bfdbee90ce01945b389fd65aecc746
jontran1 <jtran001@citymail.cuny.edu>
2020-04-07 13:35:38 -0400

    Visulize button and maze gen dfs works this is a test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const visualizeButton = document.getElementById("visualize");
 const dfs_backtracking_maze_gen_button = document.getElementById("dfs-backtracking-maze-gen");
 
 dfs_backtracking_maze_gen_button.addEventListener("click", () => {
+    console.log("clicked")
     visualizeButton.addEventListener("click", activateMazeGenerationDFS);
 })
 
