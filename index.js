da2691c8336f54a1404ef20d4a2321287b43c41f
jontran1 <jtran001@citymail.cuny.edu>
2020-04-12 17:53:13 -0400

    Created BFSButton and referenced it to the BFS link.

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const visualizeButton = document.getElementById("visualize");
 const dfs_backtracking_maze_gen_button = document.getElementById("dfs-backtracking-maze-gen");
 
 dfs_backtracking_maze_gen_button.addEventListener("click", () => {
-    console.log("clicked")
     visualizeButton.addEventListener("click", activateMazeGenerationDFS);
-})
+});
 
 const resetButton = document.getElementById("reset");
 
 resetButton.addEventListener("click", reset);
+
+
+const BFSButton = document.getElementById("BFSButton");
+
+BFSButton.addEventListener("click", () => {
+
+});
+
