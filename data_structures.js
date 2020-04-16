122d37eb881a35c387745e458027a98b9b6808c3
jontran1 <jtran001@citymail.cuny.edu>
2020-04-16 16:56:19 -0400

    Added PriorityQueue class with enqueue method

--- data_structures.js
+++ data_structures.js
@@ -56,3 +56,29 @@ class Queue {
        this.priority = priority;
     }
  }
+
+ class PriorityQueue{
+
+   constructor(){
+      this.items = [];
+   }
+
+   enqueue(element, priority){
+
+      var qElement = new QElement(element, priority);
+      var contain = false;
+
+      for(var i = 0; i < this.items.length; i++){
+         if(this.items[i].priority > qElement.priority){
+            this.items.splice(i, 0, qElement);
+            contain = true;
+            break;
+         }
+      }
+
+      if(!contain){
+         this.items.push(qElement);
+      }
+   }
+
+ }
