8082ebb355007b41d39146e7d31366510e7412ac
jontran1 <jtran001@citymail.cuny.edu>
2020-04-15 20:02:38 -0400

    Added refactored code into bredthFirstSearchSetupHelper method

--- search_algorithms.js
+++ search_algorithms.js
@@ -63,6 +63,14 @@ function breadthFirstSearchSetup(){
     visited = []; stack = [];
     queue = new Queue(rows*cols);
     queue.enqueue(startCell); 
+
+    currentAlgorithmObject.runFunction = function(){
+        if(playBFSAnimation()){
+            this.runFunction = function() {
+                DFSIter();
+            };
+        }
+    }
 }
 
 function playBFSAnimation(){
@@ -87,3 +95,24 @@ function playBFSAnimation(){
     }
 }
 
+function BFSIter(){
+    if(!queue.isEmpty()){
+        current = queue.dequeue();
+        current.highlightCell();
+        current.green = true;
+        if(current == targetCell){
+            visited.push(current);
+            noLoop();
+            playBFSAnimation = false;
+        }
+        if(!visited.includes(current)){
+            visited.push(current);
+            adjacentCells = current.adjacentCells();
+            if(adjacentCells){
+                for(let i = 0; i < adjacentCells.length; i++){
+                    queue.enqueue(adjacentCells[i]);
+                }
+            }
+        }
+    }
+}
