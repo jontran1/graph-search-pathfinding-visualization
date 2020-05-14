98fc20348540080f5d04a64c554774c60aae17bf
jontran1 <jtran001@citymail.cuny.edu>
2020-05-13 22:23:27 -0400

    I was able to get the visualize button to switch algos

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const visualizeButton = document.getElementById("visualize");
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", reset);
 
 function setVisualizeButtonAlgo(algorithm) {
+    var visualizeButton = document.getElementById("visualize");
+    var oldVisualizeButton = visualizeButton;
+    visualizeButton = oldVisualizeButton.cloneNode(true);
+    oldVisualizeButton.parentNode.replaceChild(visualizeButton, oldVisualizeButton);
     visualizeButton.addEventListener("click", algorithm);
+    
 }
 
 const dfs_backtracking_maze_gen_button = document.getElementById("dfs-backtracking-maze-gen");
-
 dfs_backtracking_maze_gen_button.addEventListener("click", () => {
-
+    setVisualizeButtonAlgo(activateMazeGenerationDFS);
 });
 
-const resetButton = document.getElementById("reset");
-
-resetButton.addEventListener("click", reset);
-
-
 const BFSButton = document.getElementById("BFSButton");
-
 BFSButton.addEventListener("click", () => {
     setVisualizeButtonAlgo(breadthFirstSearchSetup);
 });
 
+const DFSButton = document.getElementById("DFSButton");
+DFSButton.addEventListener("click", () => {
+    setVisualizeButtonAlgo(depthFirstSearchSetup);
+});
